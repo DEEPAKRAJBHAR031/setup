99896896edc9b26b44fefde243a837052c27d6b4
Deepak rajbhar <141645094+DEEPAKRAJBHAR031@users.noreply.github.com>
2025-04-03 12:01:58 +0530

    Update jenkin.sh

--- jenkin.sh
+++ jenkin.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 # Add required dependencies for the jenkins package
 sudo yum install java-17 -y
-sudo yum install jenkins -y
+sudo yum install jenkins  -y
 sudo systemctl daemon-reload
-systemctl enable --now jenkins.service
-jenkins -version
-lsof -i -n -p |grep 8080
+sudo systemctl  enable --now   jenkins.service
+jenkins -version 
+lsof -i -n -P | grep 8080
