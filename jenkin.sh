e409a7b960c74e5d2c05604bf69cd285fa49e842
Deepak rajbhar <141645094+DEEPAKRAJBHAR031@users.noreply.github.com>
2025-04-23 11:42:47 +0530

    Update jenkin.sh

--- jenkin.sh
+++ jenkin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# jenkins setup for redhat
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
